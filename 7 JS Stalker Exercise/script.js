e79c95a2ab3992f58d51084d5d950f58055c166c
Amanda <daikiraidemodaisuki@gmail.com>
2018-06-17 00:24:43 -0700

    typo

--- 7 JS Stalker Exercise/script.js
+++ 7 JS Stalker Exercise/script.js
@@ -2,4 +2,4 @@ var firstName = prompt("What is your first name?");
 var lastName = prompt("What is your last name?");
 var age = prompt("What is your age?");
 console.log("Your full name is " + firstName + " " + lastName);
-console.log("Your name is " + age);
+console.log("Your age is " + age);
