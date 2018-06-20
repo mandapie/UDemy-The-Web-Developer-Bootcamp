42004f3de155b64c2aea2166eba7d82d3f2138ba
Amanda <daikiraidemodaisuki@gmail.com>
2018-06-19 21:03:13 -0700

    added delete feature; move code into specific functions

--- 15 Todo List/script.js
+++ 15 Todo List/script.js
@@ -4,11 +4,36 @@ window.setTimeout(function () {
     var list = [];
     while (choice !== "quit") {
         if (choice === "new") {
-            list.push(prompt("Add an item!"));
+            newItem();
         }
         else if (choice === "list") {
-            console.log(list);
+            listItems();
+        }
+        else if (choice === "delete") {
+            deleteItem();
         }
         choice = prompt("What would you like to do?");
+    }
+    console.log("You quitted the app.");
+
+    function newItem() {
+        var newItem = prompt("Add an item!");
+        list.push(newItem);
+        console.log(newItem + " was added to the list.");
+    }
+    function listItems() {
+        console.log("*******");
+        list.forEach(function (item, i) {
+            console.log(i + ": " + item);
+        });
+        // for (var i = 0; i < list.length; i++) {
+        //     console.log(i + ": " + list[i]);
+        // }
+        console.log("*******");
+    }
+    function deleteItem() {
+        var index = prompt("Choose the item number to remove from list.");
+        console.log(list[index] + " is removed."); //console.log before removing so that won't print out next item instead
+        list.splice(index, 1); //from index, remove 1 item
     }
 }, 500);
